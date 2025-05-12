3661d55c4fbbb2a68a3158b30d4dc2a0b4d07769
Azat Akmyradov <azakmyradov@rklesolutions.com>
2025-05-12 11:46:41 -0400

    Update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
 // eslint.config.js
-import js from '@eslint/js';
-import svelte from 'eslint-plugin-svelte';
-import prettier from 'eslint-config-prettier';
-import globals from 'globals';
-import ts from 'typescript-eslint';
-import svelteConfig from './svelte.config.js';
+import js from "@eslint/js";
+import svelte from "eslint-plugin-svelte";
+import prettier from "eslint-config-prettier";
+import globals from "globals";
+import ts from "typescript-eslint";
+import svelteConfig from "./svelte.config.js";
 
 export default ts.config(
     js.configs.recommended,
@@ -15,25 +15,29 @@ export default ts.config(
             globals: {
                 ...globals.browser,
                 ...globals.node,
-                route: 'readonly',
-                App: 'readonly',
+                route: "readonly",
+                App: "readonly",
+                RebelScan: "readonly",
             },
         },
     },
     {
         files: [
-            'resources/**/*.svelte',
-            'resources/**/*.svelte.ts',
-            'resources/**/*.svelte.js',
-            'Modules/**/*.svelte',
-            'Modules/**/*.svelte.ts',
-            'Modules/**/*.svelte.js',
+            "resources/**/*.svelte",
+            "resources/**/*.svelte.ts",
+            "resources/**/*.svelte.js",
+            "Modules/**/*.svelte",
+            "Modules/**/*.svelte.ts",
+            "Modules/**/*.svelte.js",
+            "packages/**/*.svelte",
+            "packages/**/*.svelte.ts",
+            "packages/**/*.svelte.js",
         ],
         // See more details at: https://typescript-eslint.io/packages/parser/
         languageOptions: {
             parserOptions: {
                 projectService: true,
-                extraFileExtensions: ['.svelte'], // Add support for additional file extensions, such as .svelte
+                extraFileExtensions: [".svelte"], // Add support for additional file extensions, such as .svelte
                 parser: ts.parser,
 
                 // We recommend importing and specifying svelte.config.js.
@@ -46,18 +50,21 @@ export default ts.config(
     },
     {
         rules: {
-            '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+            ],
         },
     },
     {
         ignores: [
-            'vendor',
-            'node_modules',
-            'public',
-            'bootstrap/ssr',
-            'tailwind.config.js',
-            'resources/js/types',
-            'resources/js/ziggy.js',
+            "vendor",
+            "node_modules",
+            "public",
+            "bootstrap/ssr",
+            "tailwind.config.js",
+            "resources/js/types",
+            "resources/js/ziggy.js",
         ],
     },
     prettier,
